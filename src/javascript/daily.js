20f9176a83eea30ea1308b657b728b3532a47f5e
nicholasparada <nicholasparada@gmail.com>
2019-10-16 16:16:18 -0600

    makeEmaiArr daily

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,25 +1,35 @@
 const functions = {
+
     
-    // size: (num) => {
-    //     if (num < 0)  return "negative";
-    //     if (num < 10) return "small";
-    //     if (num < 20) return "medium";
-    //     return "large";
-    // },
+    makeEmailArr: (arr) =>{
+        console.log(arr[0]);
+        return (arr[0].toLowerCase()+"."+arr[1].toLowerCase()+"@evolveu.ca");
+        
+        return ("first.last@evolveu.ca");
+        
 
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
-
-    subtract: (num1, num2) => {
-        return num1 - num2;
-    },
-
-    assertEquals: (num1) => {
-        return `${num1} is all good`;
     }
-
 };
+//     // size: (num) => {
+//     //     if (num < 0)  return "negative";
+//     //     if (num < 10) return "small";
+//     //     if (num < 20) return "medium";
+//     //     return "large";
+//     // },
+
+//     add: (num1, num2) => {
+//         return num1 + num2;
+//     },
+
+//     subtract: (num1, num2) => {
+//         return num1 - num2;
+//     },
+
+//     assertEquals: (num1) => {
+//         return `${num1} is all good`;
+//     }
+
+// };
 
 
 
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,20 +1,40 @@
 import functions from './daily'
 
-// test('Check the sizes', () => {
-//     expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
-//     expect(functions.size(0)).toBe("small");
-//     expect(functions.size(10)).toBe("medium");
-//     expect(functions.size(15)).toBe("medium");
-//     expect(functions.size(20)).toBe("large");
-//     expect(functions.size(2000000)).toBe("large");
+// // test('Check the sizes', () => {
+// //     expect(functions.size(-1)).toBe("negative"); // Consider the edge cases
+// //     expect(functions.size(0)).toBe("small");
+// //     expect(functions.size(10)).toBe("medium");
+// //     expect(functions.size(15)).toBe("medium");
+// //     expect(functions.size(20)).toBe("large");
+// //     expect(functions.size(2000000)).toBe("large");
+// // });
+
+// test('Does that add function work?', () => {
+//     expect(functions.add(1,2)).toBe(3);
+//     expect(functions.add(101,202)).toBe(303);
 // });
 
-test('Does that add function work?', () => {
-    expect(functions.add(1,2)).toBe(3);
-    expect(functions.add(101,202)).toBe(303);
+// test('Is it all good?', () => {
+//     expect(functions.assertEquals(2)).toBe("2 is all good");
+//     expect(functions.assertEquals(101)).toBe("101 is all good");
+// });
+
+
+
+/*
+    Write a function to format an email based on an array.
+*/
+
+test('email builder from an array', () => {
+    console.log(functions);
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
 });
 
-test('Is it all good?', () => {
-    expect(functions.assertEquals(2)).toBe("2 is all good");
-    expect(functions.assertEquals(101)).toBe("101 is all good");
-});
+
+
