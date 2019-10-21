532666f413de62fc9bac30ed075bf06d867d42f9
nicholasparada <nicholasparada@gmail.com>
2019-10-20 20:58:30 -0600

    Javascript section 11 exercises

---
+++ src/udemyjsdom/UdemyExercises113.js
@@ -0,0 +1,22 @@
+//Exercise 113
+// using this array, 
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. Remove banana.
+array.shift();
+
+// 2. Sort in order. 
+array.sort();
+
+// 3. Put "Kiwi" at the end.
+array.push("Kiwi");
+
+// 4. Remove "Apples".
+array.splice(0, 1);
+
+// 5. Sort in reverse order. 
+array.reverse();
+
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
+//array2[1][1][0];
