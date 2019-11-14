16bf182da965212bd3ea39289f6815de3eaac9d5
nicholasparada <nicholasparada@gmail.com>
2019-11-13 21:11:42 -0700

    dom cards

---
+++ src/02-DOM/scripts/func.js
@@ -0,0 +1,45 @@
+const functions = {
+
+    helloworld: () => {
+        console.log("hi");
+    },
+    counter: 0,
+
+    createCard: () => {
+        functions.counter++;
+
+        let div = document.createElement("div");
+        div.setAttribute("count", functions.counter);
+        div.textContent = `count ${functions.counter}`;
+        div.setAttribute("class", "test");
+        div.innerHTML = `<p> Card Number ${functions.counter} </p>
+        <button id="idAddBefore">Add Before</button>
+            <button id="idAddAfter">Add After</button>
+            <button id="idDelete">Delete</button>`;
+        return div;
+    },
+
+    addCard: (groupCardDiv) => {
+        let newChildCard = functions.createCard();
+        groupCardDiv.appendChild(newChildCard);
+        return;
+
+    },
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+export default functions;
---
+++ src/02-DOM/scripts/man.js
@@ -0,0 +1,37 @@
+import functions from "./func.js"
+// import { isDeclaredPredicate } from "@babel/types";
+
+//console.log("hola");
+
+//MAP FUNCTION
+
+const Btn = idDoIt;
+
+console.log(idDoIt);
+Btn.addEventListener("click", () => {
+    console.log("Hello world from DOM");
+});
+
+idBigDiv.addEventListener("click", () => {
+    console.log(event.target, event.target.getAttribute("count"));
+    console.log(event.target.parentNode);
+    console.log(event.target.parentNode.children);
+
+});
+
+let click = 0;
+idAdd.addEventListener("click", () => {
+    console.log(event.target);
+    
+//  alert('haha');
+ functions.addCard(idBigDiv);
+});
+
+// let a = document.getElementById("idAdd");
+// a.onclick = function(b) {
+
+    // const div = document.createElement('div');
+    // div.textContent = click++;
+    // div.className = 'test';
+    // idBigDiv.append(div);
+// };
