4b924a33c5ea13adf8f4855ee57b589704d6f027
nicholasparada <nicholasparada@gmail.com>
2020-02-04 14:26:39 -0700

    react

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComp from './MyComp.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MyComp/>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
